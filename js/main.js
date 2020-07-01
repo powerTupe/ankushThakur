a6baec5bff92065a178f0acdbd458eabcd192002
powerTupe <prasadtupe040500@gmail.com>
2020-07-01 22:16:42 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -43,5 +43,35 @@ $('.about-area .owl-carousel').owlCarousel({
     }
 })
 
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDesc = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let location =document.querySelector('.location-timezone')
+    let icons = document.querySelector('.icon img')
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = parseFloat(position.coords.longitude);
 
+            lat = parseFloat(position.coords.latitude);
+
+            // const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `https://api.weatherbit.io/v2.0/current?lon=${long}&lat=${lat}&key=9f2e3c777e964c35b5ffcbce5a4c36ea`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const { temp, city_name } = data.data[0];
+                const { description, icon } = data.data[0].weather;
+                temperatureDegree.innerHTML = temp;
+                location.innerHTML = city_name;
+                temperatureDesc.innerHTML = description;
+                icons.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+            })
+        });
+    }
+});
 
